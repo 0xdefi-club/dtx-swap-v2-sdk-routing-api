3d470bb18211b106f3361d9023628316d381bcd6
Lint Action <lint-action@samuelmeuli.com>
2024-06-27 11:21:01 +0000

    Fix code style issues with Prettier

--- dist/constants.d.ts
+++ dist/constants.d.ts
@@ -1,19 +1,19 @@
-import { Percent } from '@uniswap/sdk-core';
-import JSBI from 'jsbi';
+import { Percent } from '@uniswap/sdk-core'
+import JSBI from 'jsbi'
 /**
  * @deprecated use FACTORY_ADDRESS_MAP instead
  */
-export declare const FACTORY_ADDRESS = "0x61b781d983f5D2134946dF130E9B62962C71b2C0";
+export declare const FACTORY_ADDRESS = '0x61b781d983f5D2134946dF130E9B62962C71b2C0'
 export declare const FACTORY_ADDRESS_MAP: {
-    [chainId: number]: string;
-};
-export declare const INIT_CODE_HASH = "0x8615843ab28b4b86b2382dca22cf14f0a6ba9e52cb006531eb574042a5b54a46";
-export declare const MINIMUM_LIQUIDITY: JSBI;
-export declare const ZERO: JSBI;
-export declare const ONE: JSBI;
-export declare const FIVE: JSBI;
-export declare const _997: JSBI;
-export declare const _1000: JSBI;
-export declare const BASIS_POINTS: JSBI;
-export declare const ZERO_PERCENT: Percent;
-export declare const ONE_HUNDRED_PERCENT: Percent;
+  [chainId: number]: string
+}
+export declare const INIT_CODE_HASH = '0x8615843ab28b4b86b2382dca22cf14f0a6ba9e52cb006531eb574042a5b54a46'
+export declare const MINIMUM_LIQUIDITY: JSBI
+export declare const ZERO: JSBI
+export declare const ONE: JSBI
+export declare const FIVE: JSBI
+export declare const _997: JSBI
+export declare const _1000: JSBI
+export declare const BASIS_POINTS: JSBI
+export declare const ZERO_PERCENT: Percent
+export declare const ONE_HUNDRED_PERCENT: Percent
