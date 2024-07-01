e3d00369f85675666d363ae8ffa9c1fd5117932d
zj kkk <npckei217@gmail.com>
2024-07-01 11:08:55 +0800

    feat:update sdk-core with taiko

--- src/constants.ts
+++ src/constants.ts
@@ -4,8 +4,8 @@ import JSBI from 'jsbi'
 /**
  * @deprecated use FACTORY_ADDRESS_MAP instead
  */
-export const FACTORY_ADDRESS = '0x61b781d983f5D2134946dF130E9B62962C71b2C0'
-
+// export const FACTORY_ADDRESS = '0x61b781d983f5D2134946dF130E9B62962C71b2C0' // base sepolia
+export const FACTORY_ADDRESS = '0x2EA9051d5a48eA2350b26306f2b959D262cf67e1' // taiko
 export const FACTORY_ADDRESS_MAP: { [chainId: number]: string } = V2_FACTORY_ADDRESSES
 
 export const INIT_CODE_HASH = '0x8615843ab28b4b86b2382dca22cf14f0a6ba9e52cb006531eb574042a5b54a46'
